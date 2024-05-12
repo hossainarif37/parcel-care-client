d0fac9e37e9ea1dbf7fdd17e9325191a3aa2274b
hossainarif37 <hossainarif4937@gmail.com>
2024-05-12 22:06:36 +0600

    feat: Added Navbar

--- src/pages/Home/components/Navbar/Navbar.tsx
+++ src/pages/Home/components/Navbar/Navbar.tsx
@@ -2,8 +2,20 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="absolute w-full md:px-28 py-5">
-            <Link to='/' className="text-4xl font-bold"><span className="text-primary">Parcel</span><span className="text-secondary">Care</span></Link>
+        <div className="w-full absolute md:px-28 py-5 flex justify-between z-10 bg-transparent">
+            {/* Logo */}
+            <div>
+                <Link to='/' className="text-4xl font-bold"><span className="text-primary">Parcel</span><span className="text-secondary">Care</span></Link>
+            </div>
+
+            {/* NavLinks */}
+            <ul className="text-white font-semibold flex justify-center items-center gap-x-10">
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact Us</li>
+                <li>About Us</li>
+                <li>Login</li>
+            </ul>
         </div>
     );
 };
