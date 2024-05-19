d3e35d75ea0be95306b1fd4bdbfab9a102e6f2c4
hossainarif37 <hossainarif4937@gmail.com>
2024-05-19 21:26:07 +0600

    feat: Integrate user authentication with API and add middleware to store configuration
    Added user authentication logic in the App component using useLazyCurrentUserQuery from the user API.
    Included a check for authToken from cookies and dispatching setUser action based on API response.
    Enhanced the Redux store configuration by appending the API middleware to the default middleware stack.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,36 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./pages/Home/components/Navbar/Navbar";
+import Cookies from "js-cookie";
+import { useLazyCurrentUserQuery } from "./redux/api/endpoints/userApi";
+import { useCallback, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { IRootState } from "./types/types";
+import { setUser } from "./redux/slices/user/userSlice";
 
 const App = () => {
+  const dispatch = useDispatch();
   const location = useLocation();
   const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
+  const { isAuthenticated } = useSelector((state: IRootState) => state.userSlice);
+
+  const token = Cookies.get('authToken');
+
+  const [getCurrentUser, { data: userData, isLoading, isError, error }] = useLazyCurrentUserQuery();
+
+
+  // Inside your component
+  const getCurrentUserMemoized = useCallback(getCurrentUser, [getCurrentUser]);
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      getCurrentUserMemoized(undefined);
+    }
+    if (userData?.success && token) {
+      dispatch(setUser({ user: userData.user, isAuthenticated: true }));
+    }
+
+  }, [getCurrentUserMemoized, userData, token, isAuthenticated, dispatch]);
+
   return (
     <div className="max-w-[1920px] mx-auto">
       {!isAuthPage && <Navbar />}
