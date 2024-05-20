e6a173b6cf160e77b125791e823200518b45c5d1
hossainarif37 <hossainarif4937@gmail.com>
2024-05-21 00:03:26 +0600

    Removed unnecessary code from App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./pages/Home/components/Navbar/Navbar";
-import Cookies from "js-cookie";
 import { useLazyCurrentUserQuery } from "./redux/api/endpoints/userApi";
-import { useCallback, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { IRootState } from "./types/types";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { setUser } from "./redux/slices/user/userSlice";
 
 const App = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const isAuthPage = location.pathname === '/login' || location.pathname === '/register';
-  const { isAuthenticated } = useSelector((state: IRootState) => state.userSlice);
 
-  const token = Cookies.get('authToken');
-
-  const [getCurrentUser, { data: userData, isLoading, isError, error }] = useLazyCurrentUserQuery();
-
-
-  // Inside your component
-  const getCurrentUserMemoized = useCallback(getCurrentUser, [getCurrentUser]);
+  const [getCurrentUser, { data: userData }] = useLazyCurrentUserQuery();
 
   useEffect(() => {
-    getCurrentUserMemoized(undefined);
+    getCurrentUser(undefined);
     dispatch(setUser({ user: userData?.user, isAuthenticated: true }));
 
-  }, [getCurrentUserMemoized, userData?.user, dispatch]);
+  }, [userData?.user, dispatch, getCurrentUser]);
 
   return (
     <div className="max-w-[1920px] mx-auto">
