fb23c0ee35be3cb22d516d4065abdd8bda7657a2
hossainarif37 <hossainarif4937@gmail.com>
2024-05-23 00:20:27 +0600

    feat: Update Sidebar with role-based dynamic menu links
    - Enhance `Sidebar` to display different menu links based on user roles (user, agent, admin) using Redux state.
    - Add `dashboardMenuLinks.ts` to define role-specific menu links.
    - Use `Link` from `react-router-dom` for navigation in `Sidebar`.

---
+++ src/constants/dashboardMenuLinks.ts
@@ -0,0 +1,66 @@
+export const userMenuLinks = [
+    {
+        title: "My Parcels",
+        path: "/dashboard/user/my-parcels",
+        icon: '',
+    },
+    {
+        title: "Book Parcel",
+        path: "/dashboard/user/book-parcel",
+        icon: '',
+    },
+    {
+        title: "Profile",
+        path: "/dashboard/user/profile",
+        icon: '',
+    },
+]
+export const agentMenuLinks = [
+    {
+        title: "Delivery List",
+        path: "/dashboard/agent/delivery-list",
+        icon: '',
+    },
+    {
+        title: "Pickup List",
+        path: "/dashboard/agent/pickup-list",
+        icon: '',
+    },
+    {
+        title: "My Reviews",
+        path: "/dashboard/agent/my-reviews",
+        icon: '',
+    },
+    {
+        title: "Profile",
+        path: "/dashboard/agent/profile",
+        icon: '',
+    },
+]
+export const adminMenuLinks = [
+    {
+        title: "Overview",
+        path: "/dashboard/admin/overview",
+        icon: '',
+    },
+    {
+        title: "All Parcels",
+        path: "/dashboard/admin/all-parcels",
+        icon: '',
+    },
+    {
+        title: "All Users",
+        path: "/dashboard/admin/all-users",
+        icon: '',
+    },
+    {
+        title: "All Agents",
+        path: "/dashboard/admin/all-agents",
+        icon: '',
+    },
+    {
+        title: "Profile",
+        path: "/dashboard/admin/profile",
+        icon: '',
+    },
+]
--- src/pages/Dashboard/components/Sidebar.tsx
+++ src/pages/Dashboard/components/Sidebar.tsx
@@ -1,8 +1,29 @@
+import { useSelector } from "react-redux";
+import { IRootState } from "../../../types/types";
+import { adminMenuLinks, agentMenuLinks, userMenuLinks } from "../../../constants/dashboardMenuLinks";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+    const { user } = useSelector((state: IRootState) => state.userSlice);
+    const isNormalUser = user?.role === 'user';
+    const isAgent = user?.role === 'agent';
+    const isAdmin = user?.role === 'admin';
+    const menuLinks = isNormalUser ? userMenuLinks :
+        isAgent ? agentMenuLinks :
+            isAdmin ? adminMenuLinks : [];
+
     return (
-        <aside className="w-48 bg-secondary text-white">
-            Sidebar
+        <aside className="w-48 bg-secondary h-screen text-white">
+            <ul>
+                {menuLinks.map((category) => (
+                    <li key={category.title} className='mb-4'>
+                        <Link to={category.path}>
+                            <span>{category.icon}</span>
+                            <span>{category.title}</span>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
         </aside>
     );
 };
