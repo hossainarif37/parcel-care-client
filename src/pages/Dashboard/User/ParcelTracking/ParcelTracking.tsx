0315a41ba96252da0429ce389b56d47dbf27e9e9
hossainarif37 <hossainarif4937@gmail.com>
2024-08-15 20:56:35 +0600

    feat: Implement parcel tracking form in ParcelTracking component
    Introduced a user-friendly form in the ParcelTracking component to allow users to easily search for parcels by their ID. This update integrates react-hook-form for seamless input management and leverages a lazy query to fetch and display parcel details, improving the tracking experience. Users can now enter a parcel ID and submit it to retrieve real-time tracking information, enhancing the functionality and interactivity of the tracking feature.

--- src/pages/Dashboard/User/ParcelTracking/ParcelTracking.tsx
+++ src/pages/Dashboard/User/ParcelTracking/ParcelTracking.tsx
@@ -1,6 +1,18 @@
+import { useLazyGetABookedParcelByIdQuery } from "@/redux/api/endpoints/parcelApi";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+    parcelId: string
+}
 
 const ParcelTracking = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm<IFormInput>();
+    const [getABookedParcelById, { data, error }] = useLazyGetABookedParcelByIdQuery();
+    const handleSearch = (data: IFormInput) => {
+        getABookedParcelById(data.parcelId).unwrap();
+    }
+
     return (
         <div className="py-10">
             <div>
@@ -11,8 +23,10 @@ const ParcelTracking = () => {
                 </div>
 
                 {/* Parcel Tracking Form */}
-                <form className="relative flex max-w-3xl mx-auto mt-12">
-                    <input type="text" className="outline-none py-5 pl-14 pr-5 w-full text-lg rounded-lg text-black-50" placeholder="Enter Parcel ID" />
+                <form onSubmit={handleSubmit(handleSearch)} className="relative flex max-w-3xl mx-auto mt-12">
+                    <input
+                        {...register('parcelId', { required: 'Parcel ID is required' })}
+                        type="text" className="outline-none py-5 pl-14 pr-5 w-full text-lg rounded-lg text-black-50" placeholder="Enter Parcel ID" />
                     <Icon icon="iconamoon:search-thin" className="absolute top-1/2 -translate-y-1/2 left-5 text-2xl text-black-100" />
                     <button type="submit" className=" text-xl font-semibold gradient py-5 px-10 text-white rounded-r-lg">Search</button>
                 </form>
