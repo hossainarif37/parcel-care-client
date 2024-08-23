70a0162d6f62c1da2ec7711f566a53833cfffd87
hossainarif37 <hossainarif4937@gmail.com>
2024-08-24 00:28:02 +0600

    feat: Add assigned parcels query and integrate into MyPickupList component
    - Added `getAssignedParcelsByAgentId` query to `parcelApi` with support for role-based filtering.
    - Integrated `getAssignedParcelsByAgentId` into `MyPickupList` component for data fetching and console logging.
    - Updated routing to include `MyPickupList` component in the agent dashboard.

--- src/pages/Dashboard/Agent/MyPickupList/MyPickupList.tsx
+++ src/pages/Dashboard/Agent/MyPickupList/MyPickupList.tsx
@@ -1,5 +1,19 @@
+import Loading from "@/components/Loading";
+import { useGetAssignedParcelsByAgentIdQuery } from "@/redux/api/endpoints/parcelApi";
+import { IRootState } from "@/types/types";
+import { useSelector } from "react-redux";
 
 const MyPickupList = () => {
+    const { user } = useSelector((state: IRootState) => state.userSlice);
+    const { data, isLoading, error } = useGetAssignedParcelsByAgentIdQuery({ agentId: user?._id, assignedRoles: 'pickup' });
+
+    if (isLoading) {
+        return <Loading paddingY="py-40" textColor="text-primary" textSize="text-4xl" />
+    }
+
+    console.log(error);
+
+    console.log(data);
     return (
         <div>
             MyPickupList
