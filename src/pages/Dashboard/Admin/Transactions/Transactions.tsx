811636d5ecfc71350b2923f1a51b4963a4d79516
hossainarif37 <hossainarif4937@gmail.com>
2024-09-01 16:28:01 +0600

    feat: add `getAllTransactionsQuery` to transactionApi and implement in Transactions component
    - Added `getAllTransactionsQuery` to `transactionApi.ts` file
    - Implemented `useGetAllTransactionsQuery` hook in Transactions.tsx
    - Included loading and error states in Transactions component

--- src/pages/Dashboard/Admin/Transactions/Transactions.tsx
+++ src/pages/Dashboard/Admin/Transactions/Transactions.tsx
@@ -1,5 +1,20 @@
+import Loading from "@/components/Loading";
+import NotFoundData from "@/components/NotFoundData";
+import { useGetAllTransactionsQuery } from "@/redux/api/endpoints/transactionApi";
 
 const Transactions = () => {
+    const { data, isLoading, error } = useGetAllTransactionsQuery(undefined);
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    if (!data) {
+        return <NotFoundData>Transaction not found</NotFoundData>
+    }
+
+    console.log(data);
+    console.log(error);
     return (
         <div>
             Transactions
